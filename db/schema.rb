272557e7fe0371a14e7125b73cde986d4b004664
Herdez <Herdez@gmail.com>
2014-12-03 19:19:41 +0000

    Added Controls Form

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141129025318) do
+ActiveRecord::Schema.define(version: 20141203184226) do
 
   create_table "companies", force: true do |t|
     t.string   "name"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20141129025318) do
     t.string   "salesperson"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "status_type"
   end
 
   create_table "users", force: true do |t|
